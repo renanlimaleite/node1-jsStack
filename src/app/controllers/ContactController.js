2ddf6872482348955732c63df200466e11489142
renanlimaleite <renan.lima.softwares@gmail.com>
2022-04-21 11:34:06 -0300

    feat: app with mocked data

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -9,12 +9,80 @@ class ContactController {
   }
 
   // show -> get one register
-  show () {}
+  async show (request, response) {
+    const { id } = request.params
+    const contact = await ContactsRepositories.findById(id)
+
+    if (!contact) {
+      return response.status(404).json({ error: 'User not found' })
+    }
+
+    return response.json(contact)
+  }
+
   // store -> to create a new register
-  store () {}
+  async store (request, response) {
+    const { name, email, phone, category_id } = request.body
+
+    if (!name) {
+      response.status(400).json({ error: 'Name is required' })
+    }
+
+    const contactExists = await ContactsRepositories.findByEmail(email)
+
+    if (contactExists) {
+      return response.status(400).json({ error: 'E-mail already exists' })
+    }
+
+    const contact = await ContactsRepositories.create({
+      name,
+      email,
+      phone,
+      category_id
+    })
+
+    return response.json(contact)
+  }
+
   // update -> to update a register
-  update () {}
+  async update (request, response) {
+    const { id } = request.params
+    const { name, email, phone, category_id } = request.body
+
+    if (!name) {
+      response.status(400).json({ error: 'Name is required' })
+    }
+
+    const contactExists = await ContactsRepositories.findById(id)
+    if (!contactExists) {
+      return response.status(400).json({ error: 'User not found' })
+    }
+
+    const contactByEmail = await ContactsRepositories.findByEmail(email)
+
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response.status(400).json({ error: 'E-mail already exists' })
+    }
+
+    const contact = await ContactsRepositories.update(id, { name, email, phone, category_id })
+
+    return response.json(contact)
+  }
   // delete -> to delete a register
+
+  async delete (request, response) {
+    const { id } = request.params
+    const contact = await ContactsRepositories.findById(id)
+
+    if (!contact) {
+      return response.status(404).json({ error: 'User not found' })
+    }
+
+    await ContactsRepositories.delete(id)
+
+    // 204 => correct, but no content
+    response.sendStatus(204)
+  }
 }
 
 module.exports = new ContactController()
--- src/app/repositories/ContactsRepositories.js
+++ src/app/repositories/ContactsRepositories.js
@@ -7,35 +7,86 @@
  *   that keeps the logic of data.
  * - Our controller should have only rules about the application itself
  */
-const { uuid } = require('uuidv4')
+const { v4 } = require('uuid')
 
-const contacts = [
+let contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: 'Renan',
     email: 'renan@gmail.com',
     phone: '21965152458',
-    category_id: uuid()
+    category_id: v4()
   },
   {
-    id: uuid(),
+    id: v4(),
     name: 'Fulano',
     email: 'fulano@gmail.com',
     phone: '21960152008',
-    category_id: uuid()
+    category_id: v4()
   },
   {
-    id: uuid(),
+    id: v4(),
     name: 'Ciclano',
     email: 'ciclano@gmail.com',
     phone: '21965004580',
-    category_id: uuid()
+    category_id: v4()
   }
 ]
 
 class ContactsRepositories {
   findAll () {
     return new Promise((resolve) => resolve(contacts))
+  }
+
+  findById (id) {
+    return new Promise((resolve) => resolve(
+      contacts.find(contact => contact.id === id)
+    ))
+  }
+
+  findByEmail (email) {
+    return new Promise((resolve) => resolve(
+      contacts.find(contact => contact.email === email)
+    ))
+  }
+
+  create ({ name, email, phone, category_id }) {
+    return new Promise((resolve) => {
+      const newContact = {
+        id: v4(),
+        name,
+        email,
+        phone,
+        category_id
+      }
+
+      contacts.push(newContact)
+
+      resolve(newContact)
+    })
+  }
+
+  update (id, { name, email, phone, category_id }) {
+    return new Promise((resolve) => {
+      const updatedContact = {
+        id,
+        name,
+        email,
+        phone,
+        category_id
+      }
+
+      contacts = contacts.map(contact => contact.id === id ? updatedContact : contact)
+
+      resolve(updatedContact)
+    })
+  }
+
+  delete (id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter(contact => contact.id !== id)
+      resolve()
+    })
   }
 }
 
