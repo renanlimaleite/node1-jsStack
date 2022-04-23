08403fc3f1a2c7fd1ed642b91b64a000fa19527f
renanlimaleite <renan.lima.softwares@gmail.com>
2022-04-23 11:58:51 -0300

    feat: categories module

---
+++ src/app/controllers/CategorieController.js
@@ -0,0 +1,79 @@
+const CategoriesRepositories = require('../repositories/CategoriesRepositories')
+
+class CategorieController {
+  async index (request, response) {
+    const { orderBy } = request.query
+
+    const categories = await CategoriesRepositories.findAll(orderBy)
+
+    return response.json(categories)
+  }
+
+  async store (request, response) {
+    const { name } = request.body
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' })
+    }
+
+    const category = await CategoriesRepositories.create({ name })
+
+    return response.status(201).json(category)
+  }
+
+  async update (request, response) {
+    const { id } = request.params
+    const { name } = request.body
+
+    const category = await CategoriesRepositories.findById(id)
+
+    const categoryByName = await CategoriesRepositories.findByName(name)
+
+    // params fe56ed42-9f24-4d37-8881-089e04e44f03
+    // categoryByName { id: 123456, name: 'Faculdade' }
+    // Encontrou categoryByName, mas os id's são diferentes, portanto não pode mudar.
+
+    if (categoryByName && categoryByName.id !== id) {
+      return response.status(400).json({ error: 'This category already exists' })
+    }
+
+    if (!category) {
+      return response.status(404).json({ error: 'Category not found' })
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' })
+    }
+
+    const categoryUpdated = await CategoriesRepositories.update(id, { name })
+
+    return response.json(categoryUpdated)
+  }
+
+  async show (request, response) {
+    const { id } = request.params
+    const category = await CategoriesRepositories.findById(id)
+
+    if (!category) {
+      return response.status(404).json({ error: 'Category not found' })
+    }
+
+    return response.status(200).json(category)
+  }
+
+  async delete (request, response) {
+    const { id } = request.params
+
+    const category = await CategoriesRepositories.findById(id)
+
+    if (!category) {
+      return response.status(404).json({ error: 'Category not found' })
+    }
+
+    await CategoriesRepositories.delete(id)
+
+    return response.sendStatus(204)
+  }
+}
+
+module.exports = new CategorieController()
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,18 @@ const { Router } = require('express')
 const router = Router()
 
 const ContactController = require('./app/controllers/ContactController')
+const CategorieController = require('./app/controllers/CategorieController')
 
 router.get('/contacts', ContactController.index)
 router.post('/contacts', ContactController.store)
 router.put('/contacts/:id', ContactController.update)
 router.get('/contacts/:id', ContactController.show)
 router.delete('/contacts/:id', ContactController.delete)
+
+router.get('/categories', CategorieController.index)
+router.post('/categories', CategorieController.store)
+router.put('/categories/:id', CategorieController.update)
+router.get('/categories/:id', CategorieController.show)
+router.delete('/categories/:id', CategorieController.delete)
 
 module.exports = router
